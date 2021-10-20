de9b4324095bb824e39bc18212531c64ac73bcbe
brogowski <bartlomiej.rogowski@degustudios.com>
2021-10-20 22:43:59 +0200

    Add tests for pull request commenter

--- src/main/java/com/degustudios/bitbucket/mergechecks/comment/PullRequestCommenterImpl.java
+++ src/main/java/com/degustudios/bitbucket/mergechecks/comment/PullRequestCommenterImpl.java
@@ -9,6 +9,8 @@ import com.atlassian.bitbucket.util.PageRequest;
 import com.atlassian.bitbucket.util.PageRequestImpl;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.degustudios.bitbucket.mergechecks.PullRequestCommenter;
+import com.google.common.collect.Lists;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
@@ -22,11 +24,11 @@ import java.util.stream.Collectors;
 @Service
 public class PullRequestCommenterImpl implements PullRequestCommenter {
     private final PullRequestService pullRequestService;
-    private final Pattern commentTextPattern = Pattern.compile("^For commit: \\[([a-f0-9]*)\\]\n.*", Pattern.MULTILINE | Pattern.DOTALL);
+    private final Pattern commentTextPattern = Pattern.compile("^For commit: \\[([a-f0-9]*)\\]\n?.*", Pattern.MULTILINE | Pattern.DOTALL);
     private final CommentService commentService;
     private final AuthenticationContext authenticationContext;
-    private int pageSize = 1;
-    private int characterLimitPerComment = 32768;
+    private final int pageSize = 25;
+    private final int characterLimitPerComment = 32768;
 
     public PullRequestCommenterImpl(@ComponentImport PullRequestService pullRequestService,
                                     @ComponentImport CommentService commentService,
@@ -38,6 +40,29 @@ public class PullRequestCommenterImpl implements PullRequestCommenter {
 
     @Override
     public void addComment(PullRequest pullRequest, String textComment) {
+        addOrReplaceComment(pullRequest, textComment, getLatestCommentActivity(getAllServiceComments(pullRequest)));
+    }
+
+    private void addOrReplaceComment(PullRequest pullRequest, String textComment, Optional<PullRequestCommentActivity> optionalActivity) {
+        if (optionalActivity.isPresent()) {
+            Comment comment = optionalActivity.get().getComment();
+            commentService.updateComment(new CommentUpdateRequest.Builder(comment.getId())
+                    .text(formatComment(pullRequest, textComment))
+                    .version(comment.getVersion())
+                    .build());
+        } else {
+            commentService.addComment(
+                    new AddCommentRequest.Builder(pullRequest, formatComment(pullRequest, textComment)).build());
+        }
+    }
+
+    private Optional<PullRequestCommentActivity> getLatestCommentActivity(Set<PullRequestCommentActivity> serviceComments) {
+        return serviceComments.stream()
+                .sorted(Comparator.comparing(PullRequestActivity::getId).reversed())
+                .findAny();
+    }
+
+    private Set<PullRequestCommentActivity> getAllServiceComments(PullRequest pullRequest) {
         ApplicationUser user = authenticationContext.getCurrentUser();
         Set<PullRequestCommentActivity> serviceComments = new HashSet<>();
         Page<PullRequestActivity> query = null;
@@ -55,26 +80,19 @@ public class PullRequestCommenterImpl implements PullRequestCommenter {
         }
         while (!query.getIsLastPage());
 
-        Optional<PullRequestCommentActivity> optionalActivity = serviceComments.stream()
-                .sorted(Comparator.comparing(PullRequestActivity::getId).reversed())
-                .findAny();
-
-        if (optionalActivity.isPresent()) {
-            Comment comment = optionalActivity.get().getComment();
-            commentService.updateComment(new CommentUpdateRequest.Builder(comment.getId())
-                    .text(formatComment(pullRequest, textComment))
-                    .version(comment.getVersion())
-                    .build());
-        } else {
-            commentService.addComment(
-                    new AddCommentRequest.Builder(pullRequest, formatComment(pullRequest, textComment)).build());
-        }
+        return serviceComments;
     }
 
     private String formatComment(PullRequest pullRequest, String textComment) {
-        return TrimTo(
-                "For commit: [" + getLatestCommit(pullRequest) + "]" + "\n" + textComment,
-                characterLimitPerComment);
+        String formattedComment = StringUtils.join(
+                Lists.newArrayList(
+                        "For commit: [" + getLatestCommit(pullRequest) + "]",
+                        textComment)
+                        .stream()
+                        .filter(x -> !x.isEmpty())
+                        .collect(Collectors.toList()),
+                "\n");
+        return TrimTo(formattedComment, characterLimitPerComment);
     }
 
     private String TrimTo(String text, int limt) {
--- src/test/java/ut/com/degustudios/bitbucket/mergechecks/comment/PullRequestCommenterImplTest.java
+++ src/test/java/ut/com/degustudios/bitbucket/mergechecks/comment/PullRequestCommenterImplTest.java
@@ -1,14 +1,186 @@
 package ut.com.degustudios.bitbucket.mergechecks.comment;
 
+import com.atlassian.bitbucket.auth.AuthenticationContext;
+import com.atlassian.bitbucket.comment.AddCommentRequest;
+import com.atlassian.bitbucket.comment.Comment;
+import com.atlassian.bitbucket.comment.CommentAction;
+import com.atlassian.bitbucket.comment.CommentService;
+import com.atlassian.bitbucket.pull.*;
+import com.atlassian.bitbucket.repository.Repository;
+import com.atlassian.bitbucket.user.ApplicationUser;
+import com.atlassian.bitbucket.util.PageImpl;
+import com.atlassian.bitbucket.util.PageRequest;
+import com.degustudios.bitbucket.mergechecks.comment.PullRequestCommenterImpl;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.text.MessageFormat;
+import java.util.stream.IntStream;
+
+import static org.mockito.Matchers.notNull;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PullRequestCommenterImplTest {
+    private static final String COMMENT_HEADER_FORMAT = "For commit: [{0}]";
+
+    @Mock
+    private PullRequestService pullRequestService;
+    @Mock
+    private CommentService commentService;
+    @Mock
+    private AuthenticationContext authenticationContext;
+    @Mock
+    private PullRequest pullRequest;
+    @Mock
+    private ApplicationUser currentUser;
+
+    private String latestHashCommentHeader;
+    private ArgumentMatcher<PullRequestActivitySearchRequest> correctSearchRequest;
+
+    @Before
+    public void initialize()
+    {
+        String commitHashFromRef = "11111111";
+        String commitHashToRef = "9999999";
+        PullRequestRef toRef = mock(PullRequestRef.class);
+        PullRequestRef fromRef = mock(PullRequestRef.class);
+        Repository repository = mock(Repository.class);
+        when(pullRequest.getToRef()).thenReturn(toRef);
+        when(pullRequest.getFromRef()).thenReturn(fromRef);
+        when(fromRef.getLatestCommit()).thenReturn(commitHashFromRef);
+        when(fromRef.getRepository()).thenReturn(repository);
+        when(toRef.getLatestCommit()).thenReturn(commitHashToRef);
+        when(toRef.getRepository()).thenReturn(repository);
+        latestHashCommentHeader = MessageFormat.format(COMMENT_HEADER_FORMAT, commitHashFromRef);
+
+        correctSearchRequest = new ArgumentMatcher<PullRequestActivitySearchRequest>() {
+            @Override
+            public boolean matches(Object o) {
+                PullRequestActivitySearchRequest searchRequest = (PullRequestActivitySearchRequest) o;
+                return searchRequest
+                        .getTypes()
+                        .equals(Sets.newHashSet(PullRequestActivityType.COMMENT))
+                        && searchRequest
+                        .getCommentActions()
+                        .equals(Sets.newHashSet(CommentAction.ADDED, CommentAction.EDITED))
+                        && searchRequest.getCommentIds().isEmpty();
+            }
+        };
+    }
 
     @Test
-    public void Test() {
+    public void canCreateEmptyComment() {
+        setupSearchToReturn(Lists.newArrayList());
 
+        addComment("");
+
+        verifyThatCommentWithTextWasAdded(latestHashCommentHeader);
+    }
+
+    @Test
+    public void createsNewComment() {
+        String comment = "COMMENT!";
+        setupSearchToReturn(Lists.newArrayList());
+
+        addComment(comment);
+
+        verifyThatCommentWithTextWasAdded(latestHashCommentHeader + "\n" + comment);
+    }
+
+    @Test
+    public void trimsCommentWhenItsTooLong() {
+        String footer = " (...)";
+        int limit = 32768;
+        int headerLength = (latestHashCommentHeader + "\n").length();
+        int footerLength = footer.length();
+        String maximumLengthComment = getStringOfLength(limit - headerLength - footerLength);
+        String tooLongString = getStringOfLength(limit*2);
+        setupSearchToReturn(Lists.newArrayList());
+
+        addComment(tooLongString);
+
+        verifyThatCommentWithTextWasAdded(latestHashCommentHeader + "\n" + maximumLengthComment + footer);
+    }
+
+    @Test
+    public void doesNotCreateNewCommentWhenThereIsAlreadyOneByCurrentUserForLatestFromRefHash() {
+        String comment = "COMMENT!";
+        setupSearchToReturn(Lists.newArrayList(getPullRequestCommentActivity(latestHashCommentHeader)));
+
+        addComment(comment);
+
+        verify(commentService, times(0)).addComment(any());
+    }
+
+    @Test
+    public void doesCreateNewCommentWhenThereIsAlreadyOneByCurrentUserButForOtherHash() {
+        String otherHashCommentHeader = MessageFormat.format(COMMENT_HEADER_FORMAT, "otherHashId");
+        setupSearchToReturn(Lists.newArrayList(getPullRequestCommentActivity(otherHashCommentHeader)));
+
+        addComment("");
+
+        verifyThatCommentWithTextWasAdded(latestHashCommentHeader);
+    }
+
+    @Test
+    public void doesCreateNewCommentWhenThereIsAlreadyOneForLatestFromRefHashButByOtherUser() {
+        ApplicationUser otherUser = mock(ApplicationUser.class);
+        setupSearchToReturn(Lists.newArrayList(getPullRequestCommentActivity(latestHashCommentHeader, otherUser)));
+
+        addComment("");
+
+        verifyThatCommentWithTextWasAdded(latestHashCommentHeader);
+    }
+
+    private void addComment(String s) {
+        new PullRequestCommenterImpl(pullRequestService, commentService, authenticationContext)
+                .addComment(pullRequest, s);
+    }
+
+    private void setupSearchToReturn(Iterable<PullRequestActivity> elements) {
+        when(pullRequestService
+                .searchActivities(argThat(correctSearchRequest), notNull(PageRequest.class)))
+                .thenAnswer(invocationOnMock ->
+                        new PageImpl<>((PageRequest) invocationOnMock.getArguments()[1], elements, true)
+                );
+    }
+
+    private void verifyThatCommentWithTextWasAdded(String text) {
+        ArgumentMatcher<AddCommentRequest> addCommentRequestMatcher = new ArgumentMatcher<AddCommentRequest>() {
+            @Override
+            public boolean matches(Object o) {
+                AddCommentRequest addCommentRequest = (AddCommentRequest) o;
+                return addCommentRequest.getText().equals(text);
+            }
+        };
+        verify(commentService).addComment(argThat(addCommentRequestMatcher));
+    }
+
+    private PullRequestActivity getPullRequestCommentActivity(String textComment) {
+        return getPullRequestCommentActivity(textComment, currentUser);
+    }
+
+    private PullRequestActivity getPullRequestCommentActivity(String textComment, ApplicationUser user) {
+        PullRequestCommentActivity activity = mock(PullRequestCommentActivity.class);
+        Comment comment = mock(Comment.class);
+        when(comment.getText()).thenReturn(textComment);
+        when(activity.getComment()).thenReturn(comment);
+        when(activity.getUser()).thenReturn(user);
+        return activity;
+    }
+
+    private String getStringOfLength(int i) {
+        return IntStream
+                .range(0, i)
+                .mapToObj(index -> "A")
+                .reduce((left, right) -> left + right)
+                .get();
     }
 }
